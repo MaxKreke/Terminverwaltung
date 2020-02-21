6fc17015a00f3b79d2b2df9b3eee09eed8529999
unknown <asdasd@asdasd.com>
2020-02-21 15:47:33 +0100

    function that corrects days and months for the timeshift constructor

--- time.h
+++ time.h
@@ -8,18 +8,19 @@ unsigned short toDay(const unsigned short month, const unsigned short day);
 class Time {
 public:
 	//Time();
-	Time(const unsigned int year, const unsigned short month, const unsigned short day, const unsigned short hour, const unsigned short minute);
+	Time(const unsigned int year, const unsigned long month, const unsigned long long day, const unsigned long long hour, const unsigned long long minute);
 	Time(const Time & other, unsigned long long timeShiftInMinutes);
+	void correct();
 	const unsigned long long toMin() const;
 	const bool operator<(const Time & other);
 	const bool operator<=(const Time & other);
 	const bool operator==(const Time & other);
 	const bool isValid() const;
 	unsigned int m_year;
-	unsigned short m_month;
-	unsigned short m_day;
-	unsigned short m_hour;
-	unsigned short m_minute;
+	unsigned long m_month;
+	unsigned long long m_day;
+	unsigned long long m_hour;
+	unsigned long long m_minute;
 };
 
 #endif //TIME_H_
